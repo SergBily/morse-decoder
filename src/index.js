5679ce47a34b78138eee3be26933f291784d46b9
Sergbily <SergBily.dev@gmail.com>
2022-02-18 12:21:52 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arr = [];
+
+	for (let i = 0, k = 10; i < expr.length; i += 10, k += 10) {
+		arr.push(expr.slice(i, k))
+	}
+	let arr2 = [];
+	arr.forEach(el => {
+		let step = 2;
+		for (let i = 0, k = step; i < el.length; i += step, k += step) {
+			arr2.push(el.slice(i, k))
+		}
+	})
+	
+	let arr3 = [];
+	for (let i = 0, k = 5; i < arr2.length; i += 5, k += 5) {
+		arr3.push(arr2.slice(i, k))
+	}
+	
+
+	let morse = arr3.map(el => {
+		if (el.includes('**')) {
+			return [' '];
+		} else {
+			let res = []
+			let remove = [];
+			el.map(el => {
+				if (el == '11') {
+					res.push('-') ;
+				} else if (el == '10') {
+					res.push('.') 
+				} else if (el == '00') {
+					remove.push('');
+				}
+			})
+			return res
+		}
+	})
+	
+	let letter = morse.map(el => {
+		return el.join('')
+	})
+
+	let resLetter = letter.map(el => {
+		
+		if (el == ' ') {
+			return ' ';
+		} else {
+			return MORSE_TABLE[el];
+		}
+	})
+
+	return resLetter.join('')
 }
 
 module.exports = {
